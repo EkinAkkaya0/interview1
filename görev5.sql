af90214420ddfae98a4d4f2b5248ee500fa8c9af
EkinAkkaya0 <76492895+EkinAkkaya0@users.noreply.github.com>
2024-06-18 12:28:14 +0300

    Update görev5.sql

--- görev5.sql
+++ görev5.sql
@@ -3,3 +3,5 @@ FROM Emp
 JOIN Dept ON Emp.DeptId = Dept.Id
 GROUP BY Dept.Name
 ORDER BY Dept.Name;
+
+--Elimizde 5 adet departman kaydı ve 10 adet çalışan kaydı olduğu için her departmanda en az 1 çalışan olduğunu varsayarsak sonuç tablomuzda 5 satır olur.
